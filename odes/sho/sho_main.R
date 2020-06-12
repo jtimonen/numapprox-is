518b8f06fe6cfe2e3fec13ce87317f2ea3136c2d
Juho Timonen <juho.timonen@aalto.fi>
2020-06-12 05:23:26 +0300

    edit sho params

--- odes/sho/sho_main.R
+++ odes/sho/sho_main.R
@@ -3,38 +3,56 @@
 library(rstan)
 library(loo)
 source("functions.R")
-model <- stan_model(file = 'sho_rk4.stan')
-#model <- stan_model(file = 'sho_rk45.stan')
-stan_data <- readRDS(file = 'simdat.rds')
+#model <- stan_model(file = 'sho_rk4.stan')
+model <- stan_model(file = 'sho_rk45.stan')
 
-# Additional data for reference method ode_integrate_rk45
-stan_data$abs_tol_REF_  <- 1.0E-10
-stan_data$rel_tol_REF_  <- 1.0E-10
-stan_data$max_iter_REF_ <- 1.0E6
+n_chains <- 4
+SIGMA    <- c(0.1, 0.2, 0.4, 0.6, 0.8, 1.0)
+S        <- length(SIGMA)
+PARETO_K <- rep(0, S)
+RUNTIMES <- matrix(0, S, n_chains)
 
-# Additional data for the fixed-step solver
-step_size <- 0.5
-stan_data <- add_interpolation_data(stan_data, step_size)
+for(i in 1:S){
+  
+  # Load data
+  sigma <- SIGMA[i]
+  fn <- paste0('data/simdata_sigma_', sigma, '.rds')
+  stan_data <- readRDS(file = fn)
+  
+  # Additional data for reference method ode_integrate_rk45
+  stan_data$abs_tol_REF_  <- 1.0E-6
+  stan_data$rel_tol_REF_  <- 1.0E-6
+  stan_data$max_iter_REF_ <- 1.0E6
+  
+  # Additional data for the fixed-step solver
+  step_size <- 0.5
+  stan_data <- add_interpolation_data(stan_data, step_size)
+  
+  # Run sampling
+  fit <- sampling(object  = model,
+                  data    = stan_data,
+                  iter    = 4000,
+                  chains  = n_chains,
+                  control = list(adapt_delta = 0.8))
+  
+  # Extract log posterior values (not Jacobian adjusted)
+  lh1 <- get_samples(fit, 'log_lik_na')
+  lh2 <- get_samples(fit, 'log_lik_na_REF_')
+  pr1 <- get_samples(fit, 'log_prior_na')
+  pr2 <- get_samples(fit, 'log_prior_na_REF_')
+  post1 <- lh1 + pr1
+  post2 <- lh2 + pr2
+  
+  # PSIS
+  out <- psis(post1 - post2)
+  pareto_k <- out$diagnostics$pareto_k
+  cat('sigma =', sigma, 'pareto_k =', pareto_k, '\n')
+  PARETO_K[i] <- pareto_k
+  
+  # Get runtime
+  runtimes <- get_elapsed_time(fit)
+  RUNTIMES[i,] <- rowSums(runtimes)
+  
+}
 
-# Run sampling
-fit <- sampling(object  = model,
-                data    = stan_data,
-                iter    = 2000,
-                chains  = 4)
-
-# Extract log posterior values (not Jacobian adjusted)
-lh1 <- get_samples(fit, 'log_lik_na')
-lh2 <- get_samples(fit, 'log_lik_na_REF_')
-pr1 <- get_samples(fit, 'log_prior_na')
-pr2 <- get_samples(fit, 'log_prior_na_REF_')
-post1 <- lh1 + pr1
-post2 <- lh2 + pr2
-
-# PSIS
-out <- psis(post1 - post2)
-pareto_k <- out$diagnostics$pareto_k
-cat('PARETO_K =', pareto_k, '\n')
-
-# Get runtime
-runtimes <-get_elapsed_time(fit)
-print(runtimes)
+print(PARETO_K)
--- odes/sho/sho_simulate.R
+++ odes/sho/sho_simulate.R
@@ -5,24 +5,33 @@ col1 <- 'gray70'
 col2 <- 'gray10'
 lwd <- 2
 theta <- 0.3
-sigma <- 0.1
-by <- 0.2
+SIGMA <- c(0.1, 0.2, 0.4, 0.6, 0.8, 1.0)
+by <- 0.5
 T_max <- 10
+data_idx <- 1
 
-# Compile simulateiio model
+# Compile simulation model
 sm <- stan_model(file='sho_simulate.stan')
 
-# Simulate data
-th <- as.array(c(theta))
-y0 <- c(1, -1)
-ts <- seq(by, T_max, by=by)
-d1 <- list(T=length(ts), y0=y0, t0=0, ts=ts, theta=th, sigma=sigma)
-f1 <- sampling(sm, data = d1, chains = 1, cores = 1, iter = 1, warmup=0, algorithm="Fixed_param")
-y_hat <- rstan::extract(f1)$y_hat
-y  <- rstan::extract(f1)$y
+for(i in 1:length(SIGMA)){
+  sigma <- SIGMA[i]
+  
+  # Simulate data
+  th <- as.array(c(theta))
+  y0 <- c(1, -1)
+  ts <- seq(by, T_max, by=by)
+  d1 <- list(T=length(ts), y0=y0, t0=0, ts=ts, theta=th, sigma=sigma)
+  f1 <- sampling(sm, data = d1, chains = 1, cores = 1, iter = 1, warmup=0, algorithm="Fixed_param")
+  y_hat <- rstan::extract(f1)$y_hat
+  y  <- rstan::extract(f1)$y
+  
+  # Save data
+  dat <- list(y=y[data_idx,,], ts=ts, t0=0, T=length(ts))
+  fn <- paste0('data/simdata_sigma_', sigma, '.rds')
+  saveRDS(dat, file=fn)
+}
 
 # Plot data
-data_idx <- 1
 ylim <- c(min(y), max(y))
 xlim <- c(min(ts), max(ts))
 par(mfrow=c(2,1))
@@ -34,6 +43,3 @@ plot(0, 0, ylim=ylim, xlim=xlim, pch=NA, xlab='t', ylab=expression(y[2]))
 lines(ts, y_hat[data_idx,,2], col=col1, lwd=lwd)
 points(ts, y[data_idx,,2], col=col2, pch=20)
 
-# Save data
-dat <- list(y=y[data_idx,,], ts=ts, t0=0, T=length(ts))
-saveRDS(dat, file='simdat.rds')
