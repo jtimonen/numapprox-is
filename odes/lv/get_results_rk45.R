114f5b7369ce03c4f237f5c8ad51a160f03b2c68
Juho Timonen <juho.timonen@aalto.fi>
2020-06-28 11:10:37 +0300

    new result codes

---
+++ odes/lv/get_results_rk45.R
@@ -0,0 +1,57 @@
+#!/usr/bin/env Rscript
+library(ggplot2)
+library(ggpubr)
+
+N_CHAINS  <- 100
+K         <- 7
+D         <- 10
+TOLERANCE <- array(0, c(D, K))
+PARETO_K  <- array(0, c(D, K))
+RUNTIME   <- array(0, c(D, K, N_CHAINS))
+
+
+for(data_idx in 1:D){
+  
+  # Read results for this dataset
+  for(i in 1:K){
+    fn <- paste0('res/rk45/rk45_dat_', data_idx, '_tol_', i ,'.rds')
+    cat(paste0('Reading file  ', fn, '... '))
+    tryCatch({
+      res <- readRDS(fn)
+      TOLERANCE[data_idx, i] <- res$tol
+      PARETO_K[data_idx, i]  <- res$pareto_k
+      RUNTIME[data_idx, i, ] <- res$runtimes
+      cat('success!\n')
+    }, error = function(e) {
+      TOLERANCE[data_idx, i] <- NA
+      PARETO_K[data_idx, i]  <- NA
+      RUNTIME[data_idx, i, ] <- rep(NA, N_CHAINS)
+      cat('failed.\n')
+    })
+  }
+}
+TOLERANCE <- log10(TOLERANCE)
+
+# Pareto K plot
+df1 <- data.frame(as.factor(as.vector(TOLERANCE)), as.vector(PARETO_K))
+colnames(df1) <- c('tol', 'pareto_k')
+p1 <- ggplot(df1, aes(x=tol, group=tol, y=pareto_k)) + geom_hline(yintercept=0.5, lty=2, col='firebrick') + 
+  geom_boxplot(col='gray20', fill='firebrick2') + 
+  theme_bw() + scale_x_discrete(labels=as.character(TOLERANCE[1,]))
+p1 <- p1 + xlab(expression(log[10](tol))) + ylab('Pareto k')
+
+# Runtime plot
+TOL <- array(0, dim(RUNTIME))
+for(s in 1:dim(TOL)[3]){
+  TOL[,,s] <- TOLERANCE
+}
+df2 <- data.frame(as.factor(as.vector(TOL)), as.vector(RUNTIME))
+colnames(df2) <- c('tol', 'run_time')
+p2 <- ggplot(df2, aes(x=tol, group=tol, y=run_time)) +
+  geom_boxplot(col='gray20', fill='firebrick2') + 
+  theme_bw() + scale_x_discrete(labels=as.character(TOLERANCE[1,]))
+p2 <- p2 + xlab(expression(log[10](tol))) + ylab('Run time per chain (s)')
+p2 <- p2 + scale_y_continuous(breaks=seq(0,60,by=10))
+
+# Combine plots
+plt <- ggarrange(p1, p2, labels='auto')
--- odes/lv/run_rk4.R
+++ odes/lv/run_rk4.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
-args     <- commandArgs(trailingOnly = TRUE)
-DATA_IDX <- as.numeric(args[1])
+#args     <- commandArgs(trailingOnly = TRUE)
+#DATA_IDX <- as.numeric(args[1])
 
 # Setup
 library(rstan)
@@ -12,35 +12,36 @@ model <- stan_model(file = 'stan/lv_rk4.stan')
 
 # Options
 ADAPT_DELTA <- 0.95
-CHAINS      <- 100
+CHAINS      <- 30
 ITER        <- 4000
-
-# Load data
-fn    <- paste0('data/dat_set_', DATA_IDX, '.rds')
-data  <- readRDS(file = fn)
-cat(paste0('Read file ', fn, '\n'))
-data$abs_tol_REF_  <- 1.0E-10
-data$rel_tol_REF_  <- 1.0E-10
-data$max_iter_REF_ <- 1.0E6
-
-# Run with different step sizes
-H <- seq(0.1, 1, by=0.1)
-L <- length(H)
-for(i in 1:L){
-  step_size <- H[i]
+for(DATA_IDX in 1:10){
   
-  # Additional data
-  new_data <- data
-  new_data <- add_interpolation_data(new_data, h = step_size)
+  # Load data
+  fn    <- paste0('data/dat_set_', DATA_IDX, '.rds')
+  data  <- readRDS(file = fn)
+  cat(paste0('Read file ', fn, '\n'))
+  data$abs_tol_REF_  <- 1.0E-10
+  data$rel_tol_REF_  <- 1.0E-10
+  data$max_iter_REF_ <- 1.0E6
   
-  # Run inference
-  res <- run_inference(model, new_data, ITER, CHAINS, ADAPT_DELTA)
-  print(res$pareto_k)
-  print(res$runtimes)
-  res$step_size <- step_size
-  fn <- paste0('res/rk4/rk4_dat_', DATA_IDX , '_step_', i ,'.rds')
-  cat(paste0('Saving to file  ', fn, '\n'))
-  saveRDS(res, fn)
+  # Run with different step sizes
+  H <- seq(0.1, 1, by=0.1)
+  L <- length(H)
+  for(i in 1:L){
+    step_size <- H[i]
+    
+    # Additional data
+    new_data <- data
+    new_data <- add_interpolation_data(new_data, h = step_size)
+    
+    # Run inference
+    res <- run_inference(model, new_data, ITER, CHAINS, ADAPT_DELTA)
+    print(res$pareto_k)
+    print(res$runtimes)
+    res$step_size <- step_size
+    fn <- paste0('res/rk4/rk4_dat_', DATA_IDX , '_step_', i ,'.rds')
+    cat(paste0('Saving to file  ', fn, '\n'))
+    saveRDS(res, fn)
+  }
+  
 }
-
-
