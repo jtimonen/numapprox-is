3356d7df5763afc9551d7591e57cd9133de9fb36
jtimonen <juho.timonen@iki.fi>
2021-11-22 19:30:20 +0200

    improve debugging

--- experiments/debug/debug.R
+++ experiments/debug/debug.R
@@ -1,10 +1,10 @@
-
 library(cmdstanr)
+set_cmdstan_path("~/Work/Research/cmdstan")
 
 dat <- readRDS(file = "gsir_data.rds")
 model <- cmdstan_model("gsir_post.stan")
-#dat$abs_tol <- 1e-6
-#dat$rel_tol <- 1e-6
+# dat$abs_tol <- 1e-6
+# dat$rel_tol <- 1e-6
 fit <- model$sample(
   data = dat,
   sig_figs = 12,
@@ -16,10 +16,10 @@ fit <- model$sample(
   iter_sampling = 1,
   chains = 1,
   save_warmup = TRUE,
-  save_latent_dynamics=TRUE
+  save_latent_dynamics = TRUE
 )
 diag <- fit$sampler_diagnostics(inc_warmup = TRUE)
-diag[,,c("treedepth__", "n_leapfrog__", "stepsize__")]
+diag[, , c("treedepth__", "n_leapfrog__", "stepsize__")]
 
 print(fit$time())
 print(fit$profiles())
