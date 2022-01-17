36ef246cd53e183304b66eb892a1f0ff8c2a99d1
jtimonen <juho.timonen@iki.fi>
2022-01-17 19:07:46 +0200

    create plotting function

--- ode-psis/R/functions.R
+++ ode-psis/R/functions.R
@@ -30,3 +30,30 @@ validate_fit <- function(setup, sampled, tols, max_num_steps, ...) {
     tuning = tuning
   )
 }
+
+# Plot all metrics
+plot_metrics <- function(rel, tols = NULL, num_steps = NULL, flat = FALSE,
+                         arrange = TRUE) {
+  library(ggpubr)
+  if (flat) {
+    nrow <- 1
+    ncol <- 5
+  } else {
+    nrow <- 2
+    ncol <- 3
+  }
+  pA <- plot_pareto_k(rel, tols = tols, num_steps = num_steps)
+  pB <- plot_r_eff(rel, tols = tols, num_steps = num_steps)
+  pC <- plot_metric(rel$times, "time", tols = tols, num_steps = num_steps) +
+    ylab("Time (s)")
+  pD <- plot_mad(rel, tols = tols, num_steps = num_steps, loglik = FALSE)
+  pE <- plot_mad(rel, tols = tols, num_steps = num_steps, loglik = TRUE)
+  plots <- list(pA, pB, pC, pD, pE)
+  if (arrange) {
+    plots <- ggpubr::ggarrange(
+      plotlist = plots, labels = "auto",
+      nrow = nrow, ncol = ncol
+    )
+  }
+  return(plots)
+}
