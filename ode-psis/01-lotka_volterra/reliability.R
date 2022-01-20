70b35f3b3f1bd22edfe40da7533aa16ece61f9f9
jtimonen <juho.timonen@iki.fi>
2022-01-20 21:51:00 +0200

    improve reliabiity script

--- ode-psis/01-lotka_volterra/reliability.R
+++ ode-psis/01-lotka_volterra/reliability.R
@@ -49,9 +49,9 @@ reliability <- function(dir, idx) {
 }
 
 # Run
-idx_1 <- 3
-idx_2 <- 3
-idx_3 <- 3
+idx_1 <- 3 # 1 and 2 fail
+idx_2 <- 1
+idx_3 <- 1
 r1 <- reliability("results_rk45", idx_1)
 r2 <- reliability("results_rk4", idx_2)
 r3 <- reliability("results_midpoint", idx_3)
@@ -60,8 +60,26 @@ r3 <- reliability("results_midpoint", idx_3)
 res <- r1
 fits <- res$fits
 reliab <- res$reliab
+plt1_A <- plot_metrics(reliab, tols = res$confs_rel)
+plt2_A <- plot_time_comparison_tol(fits, reliab, res$idx)
+plt3_A <- plot_time_comparison_tol(fits, reliab, res$idx, TRUE)
+diags_A <- get_diags_df(fits) # rhat and reff
 
-plt1 <- plot_metrics(reliab, num_steps = res$confs)
-plt2 <- plot_time_comparison_ns(fits, reliab, res$idx)
-plt3 <- plot_time_comparison_ns(fits, reliab, res$idx, TRUE)
-diags <- get_diags_df(fits) # rhat and reff
+# Plots
+res <- r2
+fits <- res$fits
+reliab <- res$reliab
+plt1_B <- plot_metrics(reliab, num_steps = res$confs_rel)
+plt2_B <- plot_time_comparison_ns(fits, reliab, res$idx)
+plt3_B <- plot_time_comparison_ns(fits, reliab, res$idx, TRUE)
+diags_B <- get_diags_df(fits) # rhat and reff
+
+
+# Plots
+res <- r3
+fits <- res$fits
+reliab <- res$reliab
+plt1_C <- plot_metrics(reliab, num_steps = res$confs_rel)
+plt2_C <- plot_time_comparison_ns(fits, reliab, res$idx)
+plt3_C <- plot_time_comparison_ns(fits, reliab, res$idx, TRUE)
+diags_C <- get_diags_df(fits) # rhat and reff
