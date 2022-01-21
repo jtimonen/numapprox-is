cb0aa12438168612bcf2e9ec50226cac24fa0c0f
jtimonen <juho.timonen@iki.fi>
2022-01-21 09:43:54 +0200

    add  times.txt

--- ode-psis/01-lotka_volterra/main.R
+++ ode-psis/01-lotka_volterra/main.R
@@ -1,9 +1,19 @@
 # Run Lotka-Volterra experiments
 
 # Sampling
+t_start <- Sys.time()
 source("sample_midpoint.R")
+t1 <- Sys.time()
 source("sample_rk4.R")
+t2 <- Sys.time()
 source("sample_rk45.R")
+t3 <- Sys.time()
 
 # PSIS
 source("reliability.R")
+t4 <- Sys.time()
+
+times <- c(t_start - t1, t2 - t1, t3 - t2, t4 - t3)
+minutes <- round(as.numeric(times / 60), 3)
+print(times)
+print(minutes)
