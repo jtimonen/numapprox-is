b6af52d18b2bab80db163ca86042df6fdc80e061
jtimonen <juho.timonen@iki.fi>
2022-01-21 21:38:54 +0200

    improve figure

--- ode-psis/01-lotka_volterra/figures.R
+++ ode-psis/01-lotka_volterra/figures.R
@@ -51,8 +51,32 @@ time_df <- function(result, ylog) {
   idx <- result$idx
   create_time_comparison_df(fits, reliab, idx, ylog)
 }
-
 ylog <- TRUE
+
+# RK45 --------------------------------------------------------------------
+
+tol_rk45 <- results[[1]]$confs[results[[1]]$idx]
+df1 <- time_df(results[[1]], ylog)
+df1$logtol <- log10(1/df1$inv_tol)
+lab1 <- expression(time[MCMC]^{RK45(tol)})
+lab2 <- expression(time[MCMC]^{RK45(0.001)} + time[IS]^{RK45(tol)})
+
+labs <- c(lab1, lab2)
+aesth <- aes(x = logtol, y = time, group = procedure, color = procedure)
+plt_A <- ggplot(df1, aesth) + geom_line() + geom_point() + theme_bw() + 
+  scale_color_discrete(labels = labs) +
+  theme(legend.position = c(0.7, 0.45), legend.title = element_blank()) +
+  scale_x_reverse(breaks=unique(round(df1$logtol))) + 
+  xlab("log10(tol)")+ ylim(3.5, 8)
+if (ylog) {
+  plt_A <- plt_A + ylab("log(time)")
+}
+
+
+# RK4 and midpoint --------------------------------------------------------
+
+ns_rk4 <- results[[2]]$confs[results[[2]]$idx]
+ns_mp <- results[[3]]$confs[results[[3]]$idx]
 df2 <- time_df(results[[2]], ylog)
 df3 <- time_df(results[[3]], ylog)
 df <- rbind(df2, df3)
@@ -60,17 +84,22 @@ method <- c(rep("rk4", nrow(df2)), rep("midpoint", nrow(df3)))
 df$method <- as.factor(method)
 df$group <- paste0(df$procedure, " (", df$method, ")")
 
-plt <- ggplot(df, aes(x = num_steps, y = time, group = group, 
-                      color = group)) +
-  geom_line() +
-  geom_point() +
-  scale_color_brewer(type = "div", palette = 5, 
-                     labels = c("a", "b", "c", "d")) +
-  theme_bw()
+lab1a <- expression(time[MCMC]^{RK4(M)})
+lab1b <- expression(time[MCMC]^{midpoint(M)})
+lab2a <- expression(time[MCMC]^{RK4(2)} + time[IS]^{RK4(M)})
+lab2b <- expression(time[MCMC]^{midpoint(3)} + time[IS]^{midpoint(M)})
+
+labs <- c(lab1b, lab1a, lab2b, lab2a)
+aesth <- aes(x = num_steps, y = time, group = group, color = group)
+plt_B <- ggplot(df, aesth) + geom_line() + geom_point() + theme_bw() + 
+  scale_color_brewer(type = "div", palette = 5, labels = labs) +
+  xlab("Number of steps M") + 
+  theme(legend.position = c(0.7, 0.45), legend.title = element_blank()) +
+  scale_x_continuous(breaks=seq(2,30,by=2)) + ylim(3.5, 8)
 if (ylog) {
-  plt <- plt + ylab("log(time)")
+  plt_B <- plt_B + ylab("log(time)")
 }
 
-lab1 <- expression(tau[1])
-lab2 <- expression(tau[2])
-labs <- c(lab1, lab2)
+# Combine
+plt <- ggarrange(plt_A, plt_B, labels = "auto")
+ggsave(plt, filename = "figures/times.pdf",  width=7.75, height=3.43)
