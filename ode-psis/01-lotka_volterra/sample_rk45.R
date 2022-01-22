ed571dcb3d4efd8c87b972063c30647d3da3969e
jtimonen <juho.timonen@iki.fi>
2022-01-22 09:57:44 +0200

    use mns=1e5 for rk45

--- ode-psis/01-lotka_volterra/sample_rk45.R
+++ ode-psis/01-lotka_volterra/sample_rk45.R
@@ -11,7 +11,7 @@ source("setup.R")
 
 # Run sampling
 tols <- c(0.01, 0.005, 0.001, 0.0005, 0.0001, 0.00005, 10^(-5:-12))
-solvers <- rk45_list(tols = tols, max_num_steps = 1e6)
+solvers <- rk45_list(tols = tols, max_num_steps = 1e5)
 fits <- model$sample_manyconf(
   t0 = dat$t0, t = dat$t, data = add_data, init = init,
   solvers = solvers, step_size = 0.1, savedir = res_dir,
