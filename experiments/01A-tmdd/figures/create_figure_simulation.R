b8f87fe218144591c5de6c8532399529b6172eb4
jtimonen <juho.timonen@iki.fi>
2022-02-28 15:22:12 +0200

    fix plotting a bit

--- experiments/01A-tmdd/figures/create_figure_simulation.R
+++ experiments/01A-tmdd/figures/create_figure_simulation.R
@@ -32,7 +32,7 @@ res_dir <- "results"
 fn <- paste0("reliability", ".rds")
 fp <- file.path(res_dir, fn)
 results <- readRDS(file = fp)
-idx_out <- 2
+idx_out <- 4
 out <- results$outputs[[idx_out]]
 idx_low <- results$start_inds[idx_out]
 fits <- out$res$fits
@@ -43,22 +43,26 @@ gq_high <- readRDS(file = rel_files[length(rel_files)])
 # Log ratios
 aaa <- as.vector(log_ratios(gq_low, gq_high))
 imax <- which(aaa == max(aaa))
-ys_low <- gq_low$extract_odesol_df(draw_inds = imax)
-ys_high <- gq_high$extract_odesol_df(draw_inds = imax)
+ys_low <- gq_low$extract_odesol_df(draw_inds = imax, include_y0 = TRUE)
+ys_high <- gq_high$extract_odesol_df(draw_inds = imax, include_y0 = TRUE)
+tol_low <- gq_low$solver$abs_tol
+tol_high <- gq_high$solver$abs_tol
+lab_low <- parse(text = paste0("y^{BDF(", tol_low, ")}"))
+lab_high <- parse(text = paste0("y^{BDF(", tol_high, ")}"))
+labs <- c(lab_low, lab_high)
 df_ys <- rbind(ys_low, ys_high)
-df_ys$method <- c(rep("low", nrow(ys_low)), rep("high", nrow(ys_high)))
+df_ys$method <- c(rep("A_low", nrow(ys_low)), rep("B_high", nrow(ys_high)))
 df_ys$method <- factor(df_ys$method) # , labels = c("LOW", "HIGH"))
 cols <- c("#1f77b4", "#ff7f0e")
-plt_maxerr <- ggplot(df_ys, aes(x = t, y = ysol, group = method, color = method)) +
+aesth <- aes(x = t, y = ysol, group = method, color = method)
+plt_maxerr <- ggplot(df_ys, aesth) +
   geom_line() +
   facet_wrap(. ~ ydim) +
   theme_bw() +
-  scale_linetype_manual(values = c(1, 2), labels = labs) +
   scale_color_manual(values = cols, labels = labs) +
-  geom_point(data = df_dat, aes(x = t, y = y), inherit.aes = FALSE) +
   ylab("Concentration") +
   xlab("t") +
-  theme(legend.title = element_blank(), legend.position = "top")
+  theme(legend.title = element_blank())
 
 # Load two fits and plot their ODE solution distribution
 idx_high <- length(fits$files)
@@ -75,7 +79,7 @@ qhigh <- fit_high$extract_odesol_df_dist(
 df_dist <- rbind(qlow, qhigh)
 tol_low <- fit_low$solver$abs_tol
 tol_high <- fit_high$solver$abs_tol
-df_dist$tol <- as.factor(rep(c(tol_low, tol_high), each = nrow(qlow)))
+df_dist$tol <- as.factor(rep(c("A_low", "B_high"), each = nrow(qlow)))
 colnames(df_dist) <- c("t", "ydim", "lower", "median", "upper", "tol")
 lab_low <- parse(text = paste0("y^{BDF(", tol_low, ")}"))
 lab_high <- parse(text = paste0("y^{BDF(", tol_high, ")}"))
@@ -88,14 +92,15 @@ plt_post <- ggplot(df_dist, aes(
   facet_wrap(. ~ ydim) +
   geom_ribbon(alpha = 0.1) +
   theme_bw() +
+  geom_point(data = df_dat, aes(x = t, y = y), inherit.aes = FALSE) +
   scale_linetype_manual(values = c(1, 2), labels = labs) +
   scale_fill_manual(values = cols, labels = labs) +
   scale_color_manual(values = cols, labels = labs) +
   geom_line(data = df_sim, aes(x = t, y = ysol), inherit.aes = FALSE) +
   ylab("Concentration") +
   xlab("t") +
-  theme(legend.title = element_blank(), legend.position = "top")
+  theme(legend.title = element_blank())
 
 # Combine
 plt <- ggarrange(plt_post, plt_maxerr, nrow = 2, labels = "auto")
-ggsave(plt, file = "figures/tmdd_simulation.pdf", width = 6.4, height = 5.3)
+ggsave(plt, file = "figures/tmdd_simulation.pdf", width = 7.4, height = 4.3)
