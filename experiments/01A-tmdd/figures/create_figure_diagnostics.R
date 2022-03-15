958f95b5738357b287e7e49d3413d9bfc2b1cfe7
jtimonen <juho.timonen@iki.fi>
2022-03-15 15:12:35 +0200

    grad_error plot

--- experiments/01A-tmdd/figures/create_figure_diagnostics.R
+++ experiments/01A-tmdd/figures/create_figure_diagnostics.R
@@ -1,9 +1,23 @@
+# Euclidean norm of vector
+euc_norm <- function(x) {
+  sqrt(sum(x * x))
+}
+
+# Diagnose gradient
+diagnose_grad <- function(model, fit, ...) {
+  model$diagnose(
+    t0 = fit$t0, t = fit$t, data = fit$data,
+    solver = fit$solver, ...
+  )
+}
+
 # Load results
 fn <- paste0("reliability", ".rds")
 fp <- file.path(res_dir, fn)
 results <- readRDS(file = fp)
 fits <- results$outputs[[1]]$res$fits # mcmc results
 
+fn_epsilon <- 1e-10 # for finite diff
 L <- length(fits$files)
 diags <- NULL
 for (j in seq_len(L)) {
@@ -14,9 +28,17 @@ for (j in seq_len(L)) {
   max_rhat <- max(smr$rhat)
   min_eb <- min(smr$ess_bulk)
   min_et <- min(smr$ess_tail)
-  diags <- rbind(diags, c(atol, time, max_rhat, min_eb, min_et))
+  g <- diagnose_grad(model, fit, init = init, epsilon = fn_epsilon)
+  lp <- g$lp
+  grad_fn <- g$gradients$finite_diff
+  grad_error <- g$gradients$error
+  en_err <- euc_norm(grad_error)
+  diags <- rbind(diags, c(atol, time, max_rhat, min_eb, min_et, lp, log(en_err)))
 }
 diags <- data.frame(diags)
-colnames(diags) <- c("tol", "runtime", "max_rhat", "min_ess_bulk", "min_ess_tail")
+colnames(diags) <- c(
+  "tol", "runtime", "max_rhat", "min_ess_bulk",
+  "min_ess_tail", "lp", "log_err_norm_init_grad"
+)
 
 # TODO: gradient diagnose
