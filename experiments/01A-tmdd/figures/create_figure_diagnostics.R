7f02994f1c9a45bce28b860c4688797c88c292aa
jtimonen <juho.timonen@iki.fi>
2022-03-15 22:15:34 +0200

    sampler diagnostics parsing function

--- experiments/01A-tmdd/figures/create_figure_diagnostics.R
+++ experiments/01A-tmdd/figures/create_figure_diagnostics.R
@@ -11,6 +11,25 @@ diagnose_grad <- function(model, fit, ...) {
   )
 }
 
+# Diagnose sampler (note: only 2 significant digits)
+diagnose_sampler <- function(fit) {
+  fn <- paste0(tempfile(), ".csv")
+  cat(fit$cmdstan_summary$stdout, file = fn)
+  r <- read.csv(file = fn, sep = "\t")
+  r <- as.list(r[5:(5 + 6), ])
+  splitter <- function(x) strsplit(x, " +")
+  r <- sapply(r, splitter)
+  unlink(fn)
+  r <- matrix(unlist(r), 7, 10, byrow = T)[, 1:4]
+  nams <- r[, 1]
+  mean <- as.numeric(r[, 2])
+  mcse <- as.numeric(r[, 3])
+  stdev <- as.numeric(r[, 4])
+  df <- data.frame(mean, mcse, stdev)
+  rownames(df) <- nams
+  df
+}
+
 # Load results
 fn <- paste0("reliability", ".rds")
 fp <- file.path(res_dir, fn)
@@ -28,7 +47,7 @@ for (j in seq_len(L)) {
   max_rhat <- max(smr$rhat)
   min_eb <- min(smr$ess_bulk)
   min_et <- min(smr$ess_tail)
-  g <- diagnose_grad(model, fit, init = init, epsilon = fn_epsilon)
+  g <- diagnose_grad(model, fit, init = 2, epsilon = fn_epsilon)
   lp <- g$lp
   grad_fn <- g$gradients$finite_diff
   grad_error <- g$gradients$error
