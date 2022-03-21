0af4a787fd0b5f0211239a8c9aa41acffc46db45
jtimonen <juho.timonen@iki.fi>
2022-03-21 10:22:38 +0200

    improve metrics plot for tmdd

--- experiments/01A-tmdd/figures/create_figure_mainresults.R
+++ experiments/01A-tmdd/figures/create_figure_mainresults.R
@@ -3,9 +3,7 @@ fn <- paste0("reliability", ".rds")
 fp <- file.path(res_dir, fn)
 results <- readRDS(file = fp)
 
-# left plot ---------------------------------------------------------------
-
-# Create plot
+# Create timing plot
 ylog <- FALSE
 df <- NULL
 lab0 <- expression(time[MCMC]^{
@@ -33,7 +31,7 @@ df$procedure <- as.factor(df$procedure)
 n_yticks <- 8
 cols <- c("#010101", "#ca0020", "#f4a582", "#92c5de", "#0571b0")
 aesth <- aes(x = logtol, y = time, group = procedure, color = procedure)
-plt_left <- ggplot(df, aesth) +
+plt_timing <- ggplot(df, aesth) +
   geom_line() +
   geom_point() +
   theme_bw() +
@@ -41,7 +39,7 @@ plt_left <- ggplot(df, aesth) +
     values = cols,
     labels = labs
   ) +
-  theme(legend.position = c(0.35, 0.82), legend.title = element_blank()) +
+  theme(legend.position = c(0.2, 0.6), legend.title = element_blank()) +
   scale_x_reverse(breaks = unique(round(df$logtol))) +
   xlab("log10(tol)") +
   ylab("time (s)") +
@@ -53,50 +51,42 @@ plt_left <- ggplot(df, aesth) +
     labels = trans_format("log10", math_format(10^.x))
   )
 
-
-# right plot --------------------------------------------------------------
-
+# Metrics plots
 out <- results$outputs
 
-# Plot metrics combining different start points
-plot_metric_combine <- function(out, metric) {
-  cols <- c("#ca0020", "#f4a582", "#92c5de", "#0571b0")
-  df <- NULL
-  for (j in 1:4) {
-    out_j <- out[[j]]
-    df_j <- get_metric_df_tol(out_j, metric)
-    df <- rbind(df, df_j)
-  }
+# Plot metrics
+plot_metric_bdf <- function(out, metric, color = "#4daf4a") {
+  df <- get_metric_df_tol(out, metric)
   aesth <- aes_string(x = "logtol", y = "value", color = "legend")
   plt <- ggplot(df, aesth) +
     geom_line() +
     geom_point() +
     theme_bw() +
     scale_x_reverse(breaks = unique(round(df$logtol))) +
+    scale_color_manual(values = color) +
     xlab("log10(tol)") +
     ylab(metric_to_ylabel(metric)) +
-    scale_color_manual(values = cols) +
-    theme(legend.title = element_blank(), legend.position = c(0.5, 0.35))
+    theme(legend.title = element_blank(), legend.position = c(0.55, 0.4),
+          panel.grid.minor = element_blank())
   return(plt)
 }
 
-plt_A <- plot_metric_combine(out, "mad_odesol") +
-  theme(legend.position = "none")
-plt_B <- plot_metric_combine(out, "max_log_ratio") +
-  geom_hline(yintercept = 1, lty = 2) +
-  theme(legend.position = "none")
-plt_C <- plot_metric_combine(out, "pareto_k") +
-  geom_hline(yintercept = 0.5, lty = 2) +
-  theme(legend.position = "none")
-plt_D <- plot_metric_combine(out, "r_eff") +
-  geom_hline(yintercept = 1, lty = 2)
-plt_right <- ggpubr::ggarrange(plt_A, plt_B, plt_C, plt_D)
+plot_bdf <- function(out, color = "#4daf4a") {
+  plt_A <- plot_metric_bdf(out, "mad_odesol", color) +
+    theme(legend.position = "none")
+  plt_B <- plot_metric_bdf(out, "max_log_ratio", color) +
+    theme(legend.position = "none")
+  plt_C <- plot_metric_bdf(out, "pareto_k", color) +
+    theme(legend.position = "none")
+  plt_D <- plot_metric_bdf(out, "r_eff", color)
+  ggpubr::ggarrange(plt_A, plt_B, plt_C, plt_D, nrow = 1)
+}
 
+p1 <- plot_bdf(out[[1]], color = cols[2])
+p2 <- plot_bdf(out[[2]], color = cols[3])
+p3 <- plot_bdf(out[[3]], color = cols[4])
+p4 <- plot_bdf(out[[4]], color = cols[5])
+plt_metrics <- ggpubr::ggarrange(p1, p2, p3, p4, ncol = 1)
 
-# combine -----------------------------------------------------------------
-
-plt <- ggpubr::ggarrange(plt_left, plt_right,
-  widths = c(0.4, 0.6), labels = "auto"
-)
-
-ggsave(plt, file = "figures/tmdd_mainresults.pdf", width = 12, height = 4.5)
+ggsave(plt_timing, file = "figures/tmdd_timing.pdf", width = 6, height = 4.5)
+ggsave(plt_metrics, file = "figures/tmdd_metrics.pdf", width = 12, height = 7.5)
