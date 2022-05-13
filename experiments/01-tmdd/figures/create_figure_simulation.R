38741cfe859d6eaf9fdea79c84ca88fa074efa41
jtimonen <juho.timonen@iki.fi>
2022-05-13 17:12:03 +0300

    resimulation of high doesnt give exact same result as during sampling in tmdd

--- experiments/01-tmdd/figures/create_figure_simulation.R
+++ experiments/01-tmdd/figures/create_figure_simulation.R
@@ -10,17 +10,21 @@ sim_sigma <- simdat$sim_sigma
 sim_params <- prior$make_params(c(sim_k, sim_sigma))
 
 # Simulate ODE solution
-t_sim <- simdat$t
+t_data <- simdat$t
+# t_out <- t_sim
+t_out <- seq(0.02, max(t_sim), by = 0.02)
 L0_sim <- simdat$L0_sim
 t0_sim <- simdat$t0_sim
 sim <- prior$gqs(
   t0 = t0_sim,
-  t = t_sim,
+  t = t_out,
   data = list(L0 = L0_sim, D = 3),
   params = sim_params,
   solver = simdat$solver_sim
 )
 P_dat <- simdat$P_obs
+os <- sim$extract_odesol()
+yout_final <- os[1, dim(os)[2], ]
 
 # Simulation ODE solution and noisy data
 ynam <- c("y1", "y2", "y3")
@@ -44,10 +48,34 @@ out <- results$outputs[[idx_out]]
 idx_low <- results$start_inds[idx_out]
 fits <- out$res$fits
 rel_files <- out$reliab$files
-gq_low <- out$reliab$base
-gq_high <- readRDS(file = rel_files[length(rel_files)])
 
-# Absolute errors
+# Load results using low accuracy
+gq_low <- out$reliab$base
+fit_low <- readRDS(fits$files[out$idx])
+fit_low$model$reinit()
+
+# Confirm that resimulation gives same result
+sim_low <- fit_low$gqs(
+  t0 = t0_sim,
+  t = t_sim
+)
+mae1 <- max_abs_odesol_diff(sim_low, gq_low)
+message("mae_low_vs_resim = ", mae1, "\n")
+
+# Load results using low accuracy
+gq_high <- readRDS(file = rel_files[length(rel_files)])
+fit_high <- readRDS(fits$files[length(fits$files)])
+fit_high$model$reinit()
+
+# Confirm that resimulation gives same result
+sim_high <- fit_high$gqs(
+  t0 = t0_sim,
+  t = t_sim
+)
+mae2 <- max_abs_odesol_diff(sim_high, gq_high)
+message("mae_high_vs_resim = ", mae2, "\n")
+
+# Absolute errors between low and high
 maes <- abs(gq_low$extract_odesol() - gq_high$extract_odesol())
 maes <- apply(maes, 1, max)
 a <- sort(maes, index.return = T, decreasing = TRUE)
